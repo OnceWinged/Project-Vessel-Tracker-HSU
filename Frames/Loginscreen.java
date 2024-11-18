502a5c43a04db73a37f2fbb2b942c0f00ce4414a
iloveMlord <ilovemilord.op@gmail.com>
2024-11-18 23:47:05 +0700

    Add some pdf

---
+++ Frames/Loginscreen.java
@@ -0,0 +1,7 @@
+/**
+ * Milan TODO:
+ * this one for the login screen frame
+ * will include log in button & Sign up button
+ * that lead to to near identical frame "log in frame"
+ * and "sign up" frame
+ **/
